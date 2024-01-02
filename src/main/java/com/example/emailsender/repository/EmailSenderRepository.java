d152ded0623dd35618c8d3c7fc6b288374059d43
Uday2107 <dudaykumar@gmail.com>
2024-01-02 13:38:50 +0530

    Revert "code for Email-Sender"
    This reverts commit 99af19888877ed723ecd0ed849e74adb56ea4e0d.

--- src/main/java/com/example/emailsender/repository/EmailSenderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.emailsender.repository;
-
-import com.example.emailsender.resource.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailSenderRepository extends MongoRepository<Product,String> {
-}
